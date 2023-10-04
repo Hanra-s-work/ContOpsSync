5849be3d53855d2e1adaf9a89580c7e566f8185d
Henry LETELLIER <henry.letellier@epitech.eu>
2023-10-04 14:48:57 +0200

    [ADD] begining of tests

---
+++ files/tests/__init__.py
@@ -0,0 +1,3 @@
+"""
+The file in charge of making sure the folder will be recognised and callable by python
+"""
---
+++ files/tests/test_cont_ops_sync.py
@@ -0,0 +1,11 @@
+"""
+File in charge of testing the program
+"""
+# tests/test_tty_ov.py
+import os
+import sys
+import pytest
+import unittest
+import unittest.mock
+from sys import stderr
+from platform import system
