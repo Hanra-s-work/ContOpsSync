9c8e8c9624ce561c78e2f39ac502267e499fa3ab
Henry LETELLIER <henry.letellier@epitech.eu>
2023-10-04 15:27:24 +0200

    [ADD] ftp installation bash script for apt based systems

---
+++ files/dev_tools/install_ftp_on_raspberry_pi.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "Updating system"
+sudo apt update
+echo "Upgrading system"
+sudo apt full-upgrade
+echo "Installing vsftpd"
+sudo apt install vsftpd -y
+echo "Enabeling vsftpd"
+sudo systemctl start vsftpd
+sudo systemctl enable vsftpd
+sudo systemctl status vsftpd
+# echo "enabeling ftp ports"
+# sudo ufw allow 20/tcp
+# sudo ufw allow 21/tcp
+echo "Configuring vftpd"
+CONFIG_FILE=/etc/vsftpd.conf
+echo "Enabeling write"
+sed -i '/^write_enable=/ s/#*\(.*\)/\1/' $CONFIG_FILE
+echo "Exposing ftp on port 22"
+sed -i '/^local_umask=/ s/#*\(.*\)/\1/' $CONFIG_FILE
+echo "Setting the local user as the ftp root owner"
+sed -i '/^chroot_local_user=/ s/#*\(.*\)/\1/' $CONFIG_FILE
+echo "Disabeling anonymous login"
+sed -i 's/^anonymous_enable=YES/anonymous_enable=NO/' $CONFIG_FILE
+echo "Creating user token"
+echo "user_sub_token=\$USER" >>$CONFIG_FILE
+echo "Setting the users home/FTP as the root's home"
+echo "local_root=/home/\$USER/FTP" >>$CONFIG_FILE
+echo "Creating FTP/my_files folder"
+mkdir -p $HOME/FTP/my_files
+echo "Setting the user as the owner of the FTP folder"
+chown a-w $HOME/FTP
+echo "Restating vsftpd service"
+sudo service vsftpd restart
+sudo systemctl status vsftpd
+echo "Script created by (c) Henry Letellier"
