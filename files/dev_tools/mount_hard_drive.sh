369f7fc0548ee3f491cf85362a9d77524913c34f
Henry LETELLIER <henry.letellier@epitech.eu>
2023-10-06 17:42:07 +0200

    [ADD] colour to mount_hard_drive

--- files/dev_tools/mount_hard_drive.sh
+++ files/dev_tools/mount_hard_drive.sh
@@ -1,46 +1,54 @@
 #!/bin/bash
-my_true=1
-my_false=0
+hl_true=1
+hl_false=0
+colour_a='\033[1;32m'
+colour_c='\033[1;31m'
+colour_e='\033[1;33m'
+colour_r='\033[0m'
+
+function cecho() {
+    cecho -e "$colour_a$1$colour_r"
+}
 
 function yn_question() {
     while true; do
-        read -p "$1 [(y)es/(N)o]" yn
+        read -p "$colour_a$1 [(y)es/(N)o]$colour_r" yn
         case $yn in
         [Yy]*)
-            return $my_true
+            return $hl_true
             break
             ;;
         [Nn]*)
-            return $my_false
+            return $hl_false
             break
             ;;
         *)
-            echo "Please answer y for yes or n for no."
+            echo -e "${colour_c}Please answer y for yes or n for no.$colour_r"
             ;;
         esac
     done
 }
 
 function ask_for_hard_drive() {
-    read -p "External hard drive name: " external_drive_name
+    read -p "${colour_a}External hard drive name: $colour_r" external_drive_name
     echo "$external_drive_name"
 }
 
 function get_hard_drive_name() {
-    echo "Displaying available hard drives: (with lsblk -f)"
+    cecho "Displaying available hard drives: (with lsblk -f)"
     sudo lsblk -f
-    echo "Please enter the name of the external hard drive:"
-    echo "Ex: sda1"
+    cecho "Please enter the name of the external hard drive:"
+    cecho "Ex: sda1"
 }
 
 function is_drive_mounted() {
     local drive_name=$1
-    local drive_mounted=$my_false
+    local drive_mounted=$hl_false
     local drive_mounted=$(df -h | grep $drive_name | wc -l)
     if [ $drive_mounted -eq 0 ]; then
-        return $my_false
+        return $hl_false
     else
-        return $my_true
+        return $hl_true
     fi
 }
 
@@ -50,9 +58,9 @@ function is_only_blanks() {
     local string_without_blanks=${string// /}
     local string_without_blanks_length=${#string_without_blanks}
     if [ $string_length -eq $string_without_blanks_length ]; then
-        return $my_false
+        return $hl_false
     else
-        return $my_true
+        return $hl_true
     fi
 }
 
@@ -60,9 +68,9 @@ function is_first_character_a_number() {
     local string=$1
     local first_character=${string:0:1}
     if [[ $first_character =~ ^[0-9]+$ ]]; then
-        return $my_true
+        return $hl_true
     else
-        return $my_false
+        return $hl_false
     fi
 }
 
@@ -72,40 +80,40 @@ function drive_has_illegal_characters() {
     local string_without_illegal_characters=${string//[^a-zA-Z0-9_-]/}
     local string_without_illegal_characters_length=${#string_without_illegal_characters}
     if [ $string_length -eq $string_without_illegal_characters_length ]; then
-        return $my_false
+        return $hl_false
     else
-        return $my_true
+        return $hl_true
     fi
 }
 
 function get_mount_name() {
     local mount_name=""
-    local blanks_status=$my_false
-    local illegal_characters_status=$my_false
-    local stop_loop=$my_false
-    while [ ${#mount_name} -eq 0 ] && [ $stop_loop -eq $my_false ]; do
-        echo "Please enter the name you wish to assign to the drive when mounted:" >&2
-        read -p "Mount name: " mount_name
-        echo "mount_name = $mount_name" >&2
+    local blanks_status=$hl_false
+    local illegal_characters_status=$hl_false
+    local stop_loop=$hl_false
+    while [ ${#mount_name} -eq 0 ] && [ $stop_loop -eq $hl_false ]; do
+        cecho "Please enter the name you wish to assign to the drive when mounted:" >&2
+        read -p "${colour_a}Mount name: $colour_r" mount_name
+        cecho "mount_name = $colour_e$mount_name" >&2
         is_only_blanks "$mount_name"
         blanks_status=$?
-        if [ $blanks_status -eq $my_true ]; then
+        if [ $blanks_status -eq $hl_true ]; then
             echo "Please enter an alphanumeric name (- and _ are also allowed)" >&2
             mount_name=""
         fi
         drive_has_illegal_characters "$mount_name"
         illegal_characters_status=$?
-        if [ $illegal_characters_status -eq $my_true ]; then
+        if [ $illegal_characters_status -eq $hl_true ]; then
             echo "Please enter an alphanumeric name (only letters and numbers) + - and _ characters" >&2
             mount_name=""
         fi
-        if [ $blanks_status -eq $my_false ] && [ $illegal_characters_status -eq $my_false ]; then
-            stop_loop=$my_true
+        if [ $blanks_status -eq $hl_false ] && [ $illegal_characters_status -eq $hl_false ]; then
+            stop_loop=$hl_true
             break
         fi
     done
     is_first_character_a_number $mount_name
-    if [ $? -eq $my_true ]; then
+    if [ $? -eq $hl_true ]; then
         mount_name="drive$mount_name"
         echo "The first character was a number, thus, the new name is: $mount_name" >&2
     fi
@@ -115,32 +123,52 @@ function get_mount_name() {
 function mount_drive() {
     local drive=$1
     local drive_name=""
-    local mount_drive=$my_false
+    local mount_drive=$hl_false
     yn_question "Do you wish to mount $drive?"
     mount_drive=$?
-    if [ $mount_drive -eq $my_true ]; then
+    if [ $mount_drive -eq $hl_true ]; then
         drive_name=$(get_mount_name)
         sudo mkdir -p /media/$USER/$drive_name
         sudo mount /dev/$drive /media/$USER/$drive_name
     fi
 }
 
+function unmount_drive() {
+    local drive=$1
+    local drive_name=""
+    local unmount_drive=$hl_false
+    yn_question "Do you wish to unmount $drive?"
+    unmount_drive=$?
+    if [ $unmount_drive -eq $hl_true ]; then
+        drive_name=$(get_mount_name)
+        sudo umount /dev/$drive
+    fi
+}
+
 function main() {
     local drive_name=""
-    local drive_mounted=$my_false
+    local drive_mounted=$hl_false
 
     get_hard_drive_name
     drive_name=$(ask_for_hard_drive)
     is_drive_mounted $drive_name
     drive_mounted=$?
-    if [ $drive_mounted -eq $my_true ]; then
+    if [ $drive_mounted -eq $hl_true ]; then
         echo "Drive $drive_name is mounted"
+        unmount_drive $drive_name
+        is_drive_mounted $drive_name
+        drive_mounted=$?
+        if [ $drive_mounted -eq $hl_true ]; then
+            echo "Drive $drive_name is mounted"
+        else
+            echo "Drive $drive_name is not mounted"
+        fi
     else
         echo "Drive $drive_name is not mounted"
         mount_drive $drive_name
         is_drive_mounted $drive_name
         drive_mounted=$?
-        if [ $drive_mounted -eq $my_true ]; then
+        if [ $drive_mounted -eq $hl_true ]; then
             echo "Drive $drive_name is mounted"
         else
             echo "Drive $drive_name is not mounted"
