b3fa6e3779164e18517b22f112b677121c63f4f5
Henry LETELLIER <henry.letellier@epitech.eu>
2023-10-19 17:04:04 +0200

    [FIX] Container build

--- files/example_scripts/hello_world_web/deploy_containerd.sh
+++ files/example_scripts/hello_world_web/deploy_containerd.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Build docker image
+docker build -t hello-world-web:latest .
+
 # Set the namespace where you want to deploy the pod
-NAMESPACE="hello-world-containerd"
+NAMESPACE="hello-world-deployment-containerd"
 
 # Path to your Deployment YAML file
-DEPLOYMENT_YAML="./hello-world-containerd.yaml"
+DEPLOYMENT_YAML="./hello-world-deployment-containerd.yaml"
+
+# Create the namespace if it doesn't exist
+kubectl get namespace "$NAMESPACE" &>/dev/null || kubectl create namespace "$NAMESPACE"
 
 # Create the deployment in the specified namespace
 kubectl apply -f "$DEPLOYMENT_YAML" -n "$NAMESPACE"
 
 # Check the status of the deployment
-kubectl rollout status deployment/hello-world-deployment -n "$NAMESPACE"
+kubectl rollout status deployment/hello-world-deployment-containerd -n "$NAMESPACE"
--- files/example_scripts/hello_world_web/deploy_docker.sh
+++ files/example_scripts/hello_world_web/deploy_docker.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Build docker image
+docker build -t hello-world-web:latest .
+
 # Set the namespace where you want to deploy the pod
-NAMESPACE="hello-world-docker"
+NAMESPACE="hello-world-deployment-docker"
 
 # Path to your Deployment YAML file
-DEPLOYMENT_YAML="./hello-world-docker.yaml"
+DEPLOYMENT_YAML="./hello-world-deployment-docker.yaml"
+
+# Create the namespace if it doesn't exist
+kubectl get namespace "$NAMESPACE" &>/dev/null || kubectl create namespace "$NAMESPACE"
 
 # Create the deployment in the specified namespace
 kubectl apply -f "$DEPLOYMENT_YAML" -n "$NAMESPACE"
 
 # Check the status of the deployment
-kubectl rollout status deployment/hello-world-deployment -n "$NAMESPACE"
+kubectl rollout status deployment/hello-world-deployment-docker -n "$NAMESPACE"
