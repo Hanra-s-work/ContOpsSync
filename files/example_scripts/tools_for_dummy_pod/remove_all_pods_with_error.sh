0b0342ab89cb0c80b5553d3c53d83623279719f6
Henry LETELLIER <henry.letellier@epitech.eu>
2023-11-02 17:07:42 +0100

    [UPDATE] remove all pods with errors

--- files/example_scripts/tools_for_dummy_pod/remove_all_pods_with_error.sh
+++ files/example_scripts/tools_for_dummy_pod/remove_all_pods_with_error.sh
@@ -1,4 +1,4 @@
 #:/bin/bash
 echo Removing all the pods that have the Failed status
-echo "Running: sudo kubectl delete pods -l app=my-app,run=broken --field-selector=status.phase=Failed"
-sudo kubectl delete pods -l app=my-app,run=broken --field-selector=status.phase=Failed
+echo "Running: sudo kubectl delete pods --selector=run=broken --field-selector=status.phase=Failed"
+sudo kubectl delete pods --selector=run=broken --field-selector=status.phase=Failed
