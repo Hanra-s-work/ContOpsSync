8c779871e3c12b9c198f6b20263f27df713b803d
Henry LETELLIER <henry.letellier@epitech.eu>
2023-11-03 14:55:19 +0100

    [ADD] nginx controller installer (version 1 of the script)

---
+++ files/example_scripts/tools_for_dummy_pod/install_nginx_controller.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+echo "Script written by (c) Henry Letellier"
+
+hl_true=0
+hl_false=1
+success=0
+error=1
+SUDO=sudo
+
+function run_command {
+    echo "Running command: $@"
+    $@
+}
+
+function yes_no {
+    while [ $hl_true -eq $hl_true ]; do
+        read -p "$1 [(Y)es/(N)o]: " yn
+        case $yn in
+        [Yy]*) return $hl_true ;;
+        [Nn]*) return $hl_false ;;
+        *) echo "Please answer yes or no." ;;
+        esac
+    done
+}
+
+function install_helm_if_not_present {
+    echo "Checking if the helm manager is present"
+    run_command "$SUDO helm version >/dev/null 2>&1"
+    if [ $? -ne $success ]; then
+        echo "Installing the helm manager"
+        run_command "$SUDO curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3"
+        if [ $? -ne $success ]; then return $?; fi
+        run_command "$SUDO chmod 700 get_helm.sh"
+        if [ $? -ne $success ]; then return $?; fi
+        run_command "$SUDO ./get_helm.sh"
+        if [ $? -ne $success ]; then return $?; fi
+        run_command "$SUDO rm get_helm.sh"
+        if [ $? -ne $success ]; then return $?; fi
+    fi
+    return $success
+}
+
+yes_no "Do you wish to install the nginx controller in your kubernetes environement?"
+RESPONSE=$?
+if [ $RESPONSE -eq $hl_false ]; then
+    echo "You have decided not to install the nginx controller"
+    echo "Exiting program"
+    exit $success
+fi
+
+echo "Installing the 'nginx-ingress' manager"
+echo "Creating the namespace 'nginx-ingress'"
+run_command "$SUDO kubectl create namespace nginx-ingress"
+if [ $? -ne $success ]; then
+    echo "Failed to create the namespace 'nginx-ingress'"
+    echo "Exiting program"
+    exit $error
+fi
+echo "Installing nginx-ingress via the helm package manager"
+install_helm_if_not_present
+if [ $? -ne $success ]; then
+    echo "Failed to install the helm manager"
+    echo "Exiting program"
+    exit $error
+fi
+echo "Adding the nginx repository to the helm manager"
+run_command "$SUDO helm repo add nginx-stable https://helm.nginx.com/stable"
+if [ $? -ne $success ]; then
+    echo "Failed to add the nginx repository to the helm manager"
+    echo "Exiting program"
+    exit $error
+fi
+echo "Updating the helm repository list"
+run_command "$SUDO helm repo update"
+if [ $? -ne $success ]; then
+    echo "Failed to update the helm repository list"
+    echo "Exiting program"
+    exit $error
+fi
+echo "Installing the nginx controller"
+run_command "$SUDO helm install nginx-ingress-controller nginx-stable/nginx-ingress --namespace nginx-ingress"
+if [ $? -ne $success ]; then
+    echo "Failed to install the nginx-ingress-controller"
+    echo "Exiting program"
+    exit $error
+fi
+echo "Checking if nginx-ingress is existent"
+run_command "$SUDO kubectl get pods -n nginx-ingress"
+if [ $? -ne $success ]; then
+    echo "Failed to find nginx-ingress"
+    echo "Exiting program"
+    exit $error
+fi
+echo "Checking if nginx-ingress is running"
+run_command "$SUDO kubectl get pods -n nginx-ingress | grep Running"
+if [ $? -ne $success ]; then
+    echo "Failed to find nginx-ingress running"
+    echo "Exiting program"
+    exit $error
+fi
+echo "Checking if nginx-ingress is ready"
+run_command "$SUDO kubectl get pods -n nginx-ingress | grep 1/1"
+if [ $? -ne $success ]; then
+    echo "Failed to find nginx-ingress ready"
+    echo "Exiting program"
+    exit $error
+fi
+echo "Checking if nginx-ingress is available"
+run_command "$SUDO kubectl get pods -n nginx-ingress | grep 1/1 | grep Running | grep 1/1"
+if [ $? -ne $success ]; then
+    echo "Failed to find nginx-ingress available"
+    echo "Exiting program"
+    exit $error
+fi
+echo "The nginx-ingress is successfully installed"
+echo "Script written by (c) Henry Letellier"
