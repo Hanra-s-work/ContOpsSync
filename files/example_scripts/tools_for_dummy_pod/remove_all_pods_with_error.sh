b89d0029fb080fcb30b4bcaf1aa42a53b12dcb58
Henry LETELLIER <henry.letellier@epitech.eu>
2023-11-03 14:46:20 +0100

    [FIX] typo in the file

--- files/example_scripts/tools_for_dummy_pod/remove_all_pods_with_error.sh
+++ files/example_scripts/tools_for_dummy_pod/remove_all_pods_with_error.sh
@@ -1,4 +1,4 @@
-#:/bin/bash
+#!/bin/bash
 echo Removing all the pods that have the Failed status
 echo "Running: sudo kubectl delete pods --selector=run=broken --field-selector=status.phase=Failed"
 sudo kubectl delete pods --selector=run=broken --field-selector=status.phase=Failed
