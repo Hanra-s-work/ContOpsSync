eca26a48348c847462cdff28a95a780843b0a630
Henry LETELLIER <henry.letellier@epitech.eu>
2024-09-30 18:32:34 +0200

    [FIX] file setup.py that was breaking the code

--- files/setup.py
+++ files/setup.py
@@ -6,15 +6,29 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8", newline="\n") as fh:
     long_description = fh.read()
-    
-with open("requirements.txt", "r", encoding="utf-8", newline="\n") as fd:
-    requirements = fd.read()
 
 setuptools.setup(
     name='ContOpsSync',
     version='1.0.0',
     packages=setuptools.find_packages(),
-    install_requires=requirements.split("\n"),
+    install_requires=[
+        "colorama == 0.4.6",
+        "prettytable == 3.9.0",
+        "requests ==2.32.2",
+        "tqdm == 4.66.1",
+        "elevate == 0.1.3",
+        "ask-question == 1.2.4",
+        "colourise-output == 1.1.3",
+        "display-tty == 1.0.1",
+        "tty-ov == 1.0.18",
+        "psutil == 5.9.5",
+        "uuid == 1.30",
+        "pytest == 7.4.2",
+        "asciimatics_overlay_ov == 1.0.2",
+        "english-words == 2.0.1",
+        "pyinstaller == 6.10.0"
+
+    ],
     author="Henry Letellier",
     author_email="henrysoftwarehouse@protonmail.com",
     description="This is a program that will allow you to run pre-set functions for managins instances like docker, docker compose, kubernetes.",
